e882908983ec54c567b38b4a7542b413f2e40d63
andrliu <dieseliu321@gmail.com>
2015-06-08 23:23:34 -0700

    Initial Commit

---
+++ FlickrSearchTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FlickrSearchTool
+//
+//  Created by Andrew Liu on 6/8/15.
+//  Copyright (c) 2015 Andrew Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FlickrSearchTool/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FlickrSearchTool
+//
+//  Created by Andrew Liu on 6/8/15.
+//  Copyright (c) 2015 Andrew Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
